f36619790d7b5c0c4d37a2d66643aafbbe11b453
0xcapp3 <cappellini.alessio@gmail.com>
2022-07-10 05:34:34 -0700

    flash and term script updated

---
+++ device/flash_run.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+echo "[=] Remember always to run with sudo!\r\n"
+
+../../RIOT/dist/tools/tapsetup/tapsetup -d
+../../RIOT/dist/tools/tapsetup/tapsetup
+
+ip a a fec0:affe::1/64 dev tapbr0
+
+make -j16 flash term
